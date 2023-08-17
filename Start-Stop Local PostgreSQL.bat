cd26e384e30e7495d985af28acb148ecf9cc0a45
RafaelPB19 <88634288+RafaelPB19@users.noreply.github.com>
2023-08-17 09:57:48 -0400

    Update Start-Stop Local PostgreSQL.bat

--- Start-Stop Local PostgreSQL.bat
+++ Start-Stop Local PostgreSQL.bat
@@ -2,6 +2,6 @@ echo off
 set /p in="Stop (0) or Start (1) your PostgreSQL database? " 
 
 if %in% == 0 echo "Stoping" && pg_ctl -D "C:\Program Files\PostgreSQL\15\data" stop
-if %in% == 1 echo "Starting" && pg_ctl -D "C:\Program Files\PostgreSQL\15\data" start
+if %in% == 1 echo "Starting" && net start postgresql-x64-15
 
 pause
